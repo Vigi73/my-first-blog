c74c082031b3794360bb965aa053c283dedce21b
Vigi73 <vigi73@mail.ru>
2018-09-21 15:07:55 +0700

    1 commit6

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list_url')
+    path('', views.post_list, name='post_list_url'),
+    path('post/<pk>/', views.post_detail, name='post_detail_url'),
 ]
